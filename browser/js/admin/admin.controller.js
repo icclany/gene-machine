76dcf1b76b90f475cd209a3a15cb2da5e2242de5
Iris <icclany@users.noreply.github.com>
2016-04-22 14:20:56 -0400

    Update admin.controller.js

--- browser/js/admin/admin.controller.js
+++ browser/js/admin/admin.controller.js
@@ -4,10 +4,6 @@ app.controller('AdminCtrl', function($scope, $state, AdminFactory, theUsers){
   $scope.goToUserSettings = function(user){
     $state.go('UserSettings', user);
   };
-
   $scope.theUsers = theUsers;
-
-  console.log('inside admin controller');
-
   $scope.deleteUser = AdminFactory.deleteUser;
 });

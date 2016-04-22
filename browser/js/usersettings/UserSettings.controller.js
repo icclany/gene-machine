95a93ee67967c6b78f5a4a5144122346448a156c
Anthony Velli <anthony.velli@gmail.com>
2016-04-22 13:32:12 -0400

    receives user & allows editing fields.

---
+++ browser/js/usersettings/UserSettings.controller.js
@@ -0,0 +1,10 @@
+app.controller('UserSettings', function($scope, $stateParams, ProductFactory){
+	"use strict"
+	$scope.user = $stateParams.user;
+	$scope.editSettings = true;
+	$scope.updateProfile = function(){
+
+	};
+
+	
+});
---
+++ browser/js/usersettings/UserSettings.state.js
@@ -0,0 +1,21 @@
+app.config(function($stateProvider){
+  "use strict";
+  $stateProvider.state('UserSettings', {
+    url: '/usersettings/',
+    templateUrl: '/js/usersettings/UserSettings.html',
+    controller: 'UserSettings',
+    params: {
+      user: null
+      },
+  	resolve: {
+      User: function($stateParams, $http){
+      	var user = $stateParams.user;
+      	$http.get('/api/users/'+user._id)
+      	.then(function(populatedUser){
+      		console.log('http response from user');
+      		console.log(populatedUser);
+      	}); 
+  		}
+  	}
+  });
+});

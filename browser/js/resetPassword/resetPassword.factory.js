a3654501eae3f713750bfaeaa83ba8de0b5ecc95
Anthony Velli <anthony.velli@gmail.com>
2016-04-25 14:05:35 -0400

    merge w master

--- browser/js/resetPassword/resetPassword.factory.js
+++ browser/js/resetPassword/resetPassword.factory.js
@@ -2,7 +2,6 @@ app.factory('ResetPasswordFact', function($http) {
     "use strict";
         
     var submitReset = function(token, password) {
-        console.log('resetting');
         return $http.put('/api/users/reset/'+token, {password: password})
             .then(function(updatedUser){
                 return updatedUser.data;

cc7e4326576c9b9ea45ea795f00208b121d2a5f5
Iris <icc.lany@gmail.com>
2016-04-26 18:51:58 -0400

    About state resolves with all products

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -1,20 +1,16 @@
 'use strict';
 
-app.config(function ($stateProvider) {
+app.config(function($stateProvider) {
 
     // Register our *about* state.
     $stateProvider.state('about', {
         url: '/about',
-        controller: 'AboutController',
-        templateUrl: 'js/about/about.html'
-    });
-
-});
-
-app.controller('AboutController', function ($scope, ProductFactory) {
-
-    ProductFactory.fetchAll().then(function(products){
-      $scope.products = products;
+        templateUrl: 'js/about/about.html',
+        resolve: {
+            allProducts: function(ProductFactory) {
+                return ProductFactory.fetchAll();
+            }
+        }
     });
 
 });

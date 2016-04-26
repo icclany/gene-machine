da65bf87714d99ae832d4ceba38888b4397a87af
Brian McGough <bpmcgough@gmail.com>
2016-04-26 17:32:52 -0400

    added newline

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -6,6 +6,7 @@ app.directive('navbar', function($rootScope, AuthService, AUTH_EVENTS, CartFacto
         templateUrl: 'js/common/directives/navbar/navbar.html',
         link: function(scope) {
 
+
             scope.items = [
                 { label: 'Home', state: 'home' },
                 { label: 'About', state: 'about' },
